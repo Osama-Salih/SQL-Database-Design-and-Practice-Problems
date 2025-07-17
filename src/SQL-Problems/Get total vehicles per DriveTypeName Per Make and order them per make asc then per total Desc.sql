91a90e378087552bf98cac6958244620646b47a7
Osama Salih <osamasalih.ca@gmail.com>
2025-07-17 10:32:14 +0300

    P18. Get total vehicles per DriveTypeName Per Make and order them per make asc then per total Desc

---
+++ src/SQL-Problems/Get total vehicles per DriveTypeName Per Make and order them per make asc then per total Desc.sql
@@ -0,0 +1,8 @@
+SELECT M.Make, D.DriveTypeName, COUNT(*) AS TotalVehicles 
+FROM VehicleDetails AS V
+JOIN Makes AS M
+ON V.MakeID = M.MakeID
+JOIN DriveTypes AS D
+ON V.DriveTypeID = D.DriveTypeID
+GROUP BY D.DriveTypeName, M.Make
+ORDER BY M.Make ASC, TotalVehicles DESC;
