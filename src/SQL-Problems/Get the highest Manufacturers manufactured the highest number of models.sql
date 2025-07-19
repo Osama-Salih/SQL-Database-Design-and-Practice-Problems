0f321107e8f5ed2ed5f43e34b929620ba32f34d4
Osama Salih <osamasalih.ca@gmail.com>
2025-07-19 14:35:07 +0300

    P48. Get the highest Manufacturers manufactured the highest number of models

---
+++ src/SQL-Problems/Get the highest Manufacturers manufactured the highest number of models.sql
@@ -0,0 +1,17 @@
+SELECT M.Make, COUNT(*) AS NumberOfModels 
+FROM Makes AS M
+JOIN MakeModels AS Mo
+ON Mo.MakeID = M.MakeID
+GROUP BY M.Make
+HAVING COUNT(*) = (
+
+		SELECT MAX(NumberOfModels) AS MaxNumberOfModels
+		FROM (
+			SELECT MakeID, COUNT(*) AS NumberOfModels
+			FROM MakeModels
+			GROUP BY MakeID
+		) AS R1);
+
+
+
+
